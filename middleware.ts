4aede0e267cbce2ec403b564c6bb3cb3e5f5a796
biobinary <mmcury2019@gmail.com>
2024-12-11 00:23:19 +0700

    menambahkan autentikasi admin dan fitur menambahkan produk ke database

--- middleware.ts
+++ middleware.ts
@@ -3,24 +3,55 @@ import type { NextRequest } from "next/server";
 import { jwtVerify } from "jose";
 
 export async function middleware(request: NextRequest) {
-  console.log("Middleware triggered for:", request.url);
 
   const token = request.cookies.get("token")?.value;
 
   if (token) {
+
     try {
+
       const secret = new TextEncoder().encode(process.env.JWT_SECRET);
-      await jwtVerify(token, secret);
-      return NextResponse.redirect(new URL("/", request.url));
+      const { payload } = await jwtVerify(token, secret);
+
+      if (request.nextUrl.pathname.startsWith("/auth") && payload) {
+        return NextResponse.redirect(new URL("/", request.url));
+      }
+
+      if (request.nextUrl.pathname.startsWith("/admin")) {
+        if (payload.role !== "admin") {
+          return NextResponse.json(
+            { message: 'Access forbidden' },
+            { status: 403 }
+          );
+        }
+      }
+
+      return NextResponse.next();
       
     } catch (error) {
+      
       console.error("Invalid token:", error);
+
+      if (request.nextUrl.pathname.startsWith("/admin")) {
+        return NextResponse.redirect(new URL("/auth/login", request.url));
+      }
+  
+      return NextResponse.next();
+
     }
+
+  } else {
+
+    if (request.nextUrl.pathname.startsWith("/admin")) {
+      return NextResponse.redirect(new URL("/auth/login", request.url));
+    }
+
+    return NextResponse.next();
+
   }
 
-  return NextResponse.next();
 }
 
 export const config = {
-  matcher: ["/auth/login", "/auth/register"],
+  matcher: ["/auth/login", "/auth/register", "/admin/:path*"],
 };
