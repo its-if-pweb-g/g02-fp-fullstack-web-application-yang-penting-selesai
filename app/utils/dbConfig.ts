253277dd2e432b90a67dd372eaac013b366f445d
biobinary <mmcury2019@gmail.com>
2024-12-11 18:58:08 +0700

    Update dbConfig.ts

--- app/utils/dbConfig.ts
+++ app/utils/dbConfig.ts
@@ -3,8 +3,7 @@ import { MongoClient } from "mongodb";
 // Ini bisa langsung diubah disini atau
 // tambahkan connection string MongoDB lewat file .env (rekomen)
 // const uri = process.env.MONGODB_URI || "";
-const uri =
-  "mongodb://admin-db-ecommerce:SecurePass123456789@35.193.181.126:5050/ecommerce?authSource=admin";
+const uri = "";
 
 const options = {};
 
