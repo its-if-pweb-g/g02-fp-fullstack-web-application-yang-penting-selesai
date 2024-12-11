76d547c007f1ff182d74bd3ee77bc532ff76a619
Ilaa <crossbarbar@gmail.com>
2024-12-11 15:02:55 +0700

    db config baru

--- app/utils/dbConfig.ts
+++ app/utils/dbConfig.ts
@@ -1,6 +1,7 @@
 import { MongoClient } from "mongodb";
 
-const uri = "mongodb://user-fp-pweb:itssurabaya@35.193.181.126:27017/ecommerce?authSource=admin";
+const uri =
+  "mongodb://user-fp-pweb:itssurabaya@35.193.181.126:27017/ecommerce?authSource=admin";
 const options = {};
 
 let client: MongoClient;
